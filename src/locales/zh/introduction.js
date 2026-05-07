7ecf4e8e9e45854be2b0ff263cbdea0dabe63db7
Koorye <a1311586225@gmail.com>
2026-05-07 17:02:33 +0800

    update

--- src/locales/zh/introduction.js
+++ src/locales/zh/introduction.js
@@ -4,6 +4,7 @@ export default {
     'data': [
         '你好！我是一名就读于<a href="https://www.uestc.edu.cn/" target="_blank" class="highlight-link">电子科技大学（UESTC）</a><a href="https://www.scse.uestc.edu.cn/" target="_blank" class="highlight-link">计算机科学与工程学院</a>的硕士研究生。',
         '我的研究兴趣包括<span class="highlight-red">具身智能（Embodied Intelligence）</span>和<span class="highlight-red">视觉-语言模型（Vision-Language Models）</span>。',
+        '我已在顶级会议如<span class="highlight-red">CVPR</span>、<span class="highlight-red">ICCV</span>和<span class="highlight-red">ICRA</span>上发表多篇论文，开源数据集下载量超<span class="highlight-red">4,000,000</span>次，担任<span class="highlight-red">NeurIPS</span>、<span class="highlight-red">BMVC</span>等国际会议审稿人。',
         '欢迎通过电子邮件与我联系，进行任何咨询或合作！'
     ]
 }
